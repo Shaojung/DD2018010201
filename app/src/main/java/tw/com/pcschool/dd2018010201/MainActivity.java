569c5b589c3e2b44abb441fdbcde57a9613bff84
Teacher <shaojung@gmail.com>
2018-01-02 10:52:33 +0800

    use on button function

--- app/src/main/java/tw/com/pcschool/dd2018010201/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010201/MainActivity.java
@@ -3,14 +3,18 @@ package tw.com.pcschool.dd2018010201;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn3 = (Button) findViewById(R.id.button3);
+        MyListener listener = new MyListener();
+        btn3.setOnClickListener(listener);
     }
     public void click1(View v)
     {
@@ -23,7 +27,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Test2", Toast.LENGTH_SHORT).show();
                 break;
         }
-
     }
 
+    class MyListener implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "Test3", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
